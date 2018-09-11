7605cf90977ea79b3119aaa72a1a9f26233f80ef
David Lemus <dlemusg@eafit.edu.co>
2018-09-10 19:16:55 -0500

    Outh0 implementando, falta logout

--- yourWay/models/user.js
+++ yourWay/models/user.js
@@ -1,55 +1,13 @@
 var mongoose = require('mongoose');
-var bcrypt = require('bcryptjs');
 
-// User Schema
-var UserSchema = mongoose.Schema({
-	username: {
-		type: String,
-		index:true
-	},
-	password: {
-		type: String
-	},
-	email: {
-		type: String
-	},
-	name: {
-		type: String
-	},
-	date: {
-        type: Date,
-        required: true,
-        default: Date.now
-	}
-},{	
-	
-	 collection: "users" 
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    username: String,
+    auth0Id: String,
 });
 
-var User = module.exports = mongoose.model('User', UserSchema);
+const User = mongoose.model('user', userSchema);
 
-module.exports.createUser = function(newUser, callback){
-	bcrypt.genSalt(10, function(err, salt) {
-	    bcrypt.hash(newUser.password, salt, function(err, hash) {
-	        newUser.password = hash;
-	        newUser.save(callback);
-	    });
-	});
-}
+module.exports = User;
 
-module.exports.getUserByUsername = function(username, callback){
-	var query = {username: username};
-	User.findOne(query, callback);
-}
-
-
-module.exports.getUserById = function(id, callback){
-	User.findById(id, callback);
-}
-
-module.exports.comparePassword = function(candidatePassword, hash, callback){
-	bcrypt.compare(candidatePassword, hash, function(err, isMatch) {
-    	if(err) throw err;
-    	callback(null, isMatch);
-	});
-}
