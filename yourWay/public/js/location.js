f906ffd941eb226f86b3670cfdea9a5ad2cf45f4
jrodri92 <jrodri92@eafit.edu.co>
2018-09-17 16:14:21 -0500

    ahora solo se guarda si las coordenadas son diferentes, para noguar dar el mismo punto siempre

--- yourWay/public/js/location.js
+++ yourWay/public/js/location.js
@@ -3,6 +3,8 @@ $(function () {
   const URI = '/users/location';
   var init = false;
   var ciclo;
+  var lat = 0;
+  var lon = 0;
 
   function initMap() {
     var options = {
@@ -22,6 +24,7 @@ $(function () {
   initMap();
   $('#iniciar').on('click', () => {
     iniciar();
+    console.log("se inició");
   });
 
   $('#parar').on('click', () => {
@@ -33,19 +36,29 @@ $(function () {
     getLocation();
     let latitud = document.form.latitude.value; 
     let longitud = document.form.longitude.value;
-
+    var Dlatitud = parseFloat(latitud) - parseFloat(lat);
+    var Dlongitud = parseFloat(longitud) - parseFloat(lon);
+    console.log("Empezo la locacion");
     if(latitud != ""){
-      $.ajax({
-        url: URI,
-        method: 'POST',
-        data: {
-          latitude: latitud,
-          longitude: longitud
-        },
-        error: function (err) {
-          console.log(err);
-        }
-      });
+      console.log("entré");
+      if(Dlatitud != 0 || Dlongitud != 0){
+        $.ajax({
+          url: URI,
+          method: 'POST',
+          data: {
+            latitude: latitud,
+            longitude: longitud
+          },
+          success: function() {
+            addMacker(latitude.value,longitude.value);
+            lat = latitude.value;
+            lon = longitude.value;
+          },
+          error: function (err) {
+            console.log(err);
+          }
+        });
+      }
     }
   });
   function intervalo(){
@@ -80,6 +93,6 @@ $(function () {
       x.innerHTML = "Latitude: " + position.coords.latitude + "<br>Longitude: " + position.coords.longitude;
       document.getElementById("latitude").value = position.coords.latitude;
       document.getElementById("longitude").value = position.coords.longitude;
-      addMacker(parseFloat(position.coords.latitude), position.coords.longitude);
+      //addMacker(parseFloat(position.coords.latitude), position.coords.longitude);
   }
 });
